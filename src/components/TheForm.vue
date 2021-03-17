2428d33df108350c05ad65de9e194e7be205ba23
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-17 12:58:23 +0700

    v-model & dropdown

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -57,16 +57,20 @@ export default {
   data() {
     return {
       userName: '',
-      userAge: null
+      userAge: null,
+      referrer: ''
     }
   },
   methods: {
     submitForm() {
       console.log(`User name: ${this.userName}`);
       this.userName = '';
-      
+
       console.log(`User age: ${this.userAge + 4}`);
       this.userAge = null;
+
+      console.log(`Referrer: ${this.referrer}`);
+      this.referrer = '';
     }
   },
 }
