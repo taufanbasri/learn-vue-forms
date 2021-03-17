51cea707617656ecdb06b7b2375e7bb6cfa25888
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-17 13:11:36 +0700

    v-model with checkboxes & radio button

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -19,32 +19,36 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="interest" />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorial" v-model="interest" />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="interest" />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" value="video" v-model="how" />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how" />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" value="other" v-model="how" />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class="form-control">
+      <input type="checkbox" name="confirm-terms" id="confirm-terms" v-model="confirm">
+      <label for="confirm-terms">Agree to term of user?</label>
     </div>
     <div>
       <button>Save Data</button>
@@ -58,7 +62,10 @@ export default {
     return {
       userName: '',
       userAge: null,
-      referrer: ''
+      referrer: '',
+      interest: [],
+      how: null,
+      confirm: false
     }
   },
   methods: {
@@ -71,6 +78,18 @@ export default {
 
       console.log(`Referrer: ${this.referrer}`);
       this.referrer = '';
+
+      console.log('Checkboxes:');
+      console.log(this.interest);
+      this.interest = [];
+
+      console.log('Radio:');
+      console.log(this.how);
+      this.how = null;
+
+      console.log('Confirm?');
+      console.log(this.confirm);
+      this.confirm = false;
     }
   },
 }
